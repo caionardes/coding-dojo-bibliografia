1f1de160c98efccaf4701ee8c0fcdd061c529024
caionardesgft <caio.nardes@gft.com>
2018-11-29 15:45:51 -0200

    Tela inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('./bibliografias');
+
+var porta = process.env.PORT || 8080;
+
+var server = http.createServer(function (req, res){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(“<h1>Seja bem-vindo!</h1>”);
+        res.end();
+  });
+server.listen(porta);
